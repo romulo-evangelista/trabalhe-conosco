88e4aa27d7c804d524289482294a371be70ad515
Rômulo Evangelista Ferreira <romuloevangelista2011@gmail.com>
2024-09-08 16:00:03 -0300

    feat: add farmer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FarmerModule } from './farmer/farmer.module';
 
 @Module({
-  imports: [],
+  imports: [FarmerModule],
   controllers: [AppController],
   providers: [AppService],
 })
